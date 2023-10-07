5a88b6d2dfdf0fee40fbe988c65031836e4e4bd1
Oluwaseun <adunmofestus@gmail.com>
2023-10-07 16:30:07 +0100

    click function added

---
+++ script/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("proj1").innerHTML = "The weather application will provide users with real-time weather information, forecasts, and other weather-related data, which can help them make better decisions about their day-to-day activities.";
+  }
+  function myFunction2() {
+    document.getElementById("proj2").innerHTML = "Notepad is a basic text editor that you can use to create simple documents."
+  }
+
+ 
